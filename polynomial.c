c153892895b0c5a94340723735c78e62f599ece5
shaheen-senpai <shaheen50053@gmail.com>
2023-09-19 09:55:20 +0530

    1polynomial read and printed its coefficients

---
+++ polynomial.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main()
+{
+int x6=0,x5=0,x4=0,x3=0,x2=0,x=0,c;
+printf("Enter the first string\n");
+scanf("%dx^6+%dx^5+%dx^4+%dx^3+%dx^2+%dx+%d",&x6,&x5,&x4,&x3,&x2,&x,&c);
+printf("%d %d %d %d %d %d %d",x6,x5,x4,x3,x2,x,c);
+}
