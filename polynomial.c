5ba4b1e84ed98e7916fb73f353f75e73fae6a572
shaheen-senpai <shaheen50053@gmail.com>
2023-09-19 11:38:35 +0530

    with x added

--- polynomial.c
+++ polynomial.c
@@ -1,22 +1,47 @@
 #include<stdio.h>
-main()
+#include<string.h>
+void main()
 {
-int p2[10],p1[10],i;
-char a[100],b[100];
-printf("enter the first string");
-gets(a);
-puts(a);
-printf("%d\n",a[3]);
-for(i=0;i<100;i++)
-{
-	if(a[i]='x')
-	{
-		p1[a[i+2]]=a[i-1];
-	}
-}	
+int p2[100],p1[100],p3[100],i;
+char a[100],b[100],j;
 
-for(i=0;i!='\0';i++)
+printf("enter the first string\n");
+scanf("%s",a);
+printf("enter the second string\n");
+scanf("%s",b);
+for(i=0;i<10;i++)
 {
-	printf("%d\t",p1[i]);
+	p1[i]=0;
+	p2[i]=0;
 }
+for(i=0;i<strlen(a);i++)
+{
+	if(a[i]=='x')
+	{
+		j=(int)a[i+2]-48;
+		p1[j]=(int)a[i-1]-48;
+		
+	}
+}
+for(i=0;i<strlen(b);i++)
+{
+	if(b[i]=='x')
+	{
+		j=(int)b[i+2]-48;
+		p2[j]=(int)b[i-1]-48;
+		
+	}
+
+}
+for(i=0;i<10;i++)
+{
+if(p1[i]+p2[i]!=0)
+{
+printf("%dx^%d+",p1[i]+p2[i],i);
+}
+else
+continue;
+}
+
+
 }
