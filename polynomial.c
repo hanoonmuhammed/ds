5d6a57ce6a753e84749077b35614efa9492d2240
shaheen-senpai <shaheen50053@gmail.com>
2023-09-19 11:56:26 +0530

    18/10

--- polynomial.c
+++ polynomial.c
@@ -6,9 +6,9 @@ int p2[100],p1[100],p3[100],i;
 char a[100],b[100],j;
 
 printf("enter the first string\n");
-scanf("%s",a);
+gets(a);
 printf("enter the second string\n");
-scanf("%s",b);
+gets(b);
 for(i=0;i<10;i++)
 {
 	p1[i]=0;
@@ -22,6 +22,10 @@ for(i=0;i<strlen(a);i++)
 		p1[j]=(int)a[i-1]-48;
 		
 	}
+	else if(a[i]=='\0')
+	{
+		p1[0]=a[i-1];
+	}	
 }
 for(i=0;i<strlen(b);i++)
 {
@@ -31,17 +35,18 @@ for(i=0;i<strlen(b);i++)
 		p2[j]=(int)b[i-1]-48;
 		
 	}
-
+	else if(b[i]=='\0')
+	{
+		p2[0]=b[i-1];
+	}
 }
-for(i=0;i<10;i++)
+for(i=1;i<10;i++)
 {
 if(p1[i]+p2[i]!=0)
 {
 printf("%dx^%d+",p1[i]+p2[i],i);
 }
-else
-continue;
 }
-
+printf("%d",p1[0]+p2[0]);
 
 }
